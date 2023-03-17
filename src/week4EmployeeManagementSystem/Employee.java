69f5322a49ae35db7b66d72a80bfa847b225e931
abhaygupta3 <abhaygupta030902@gmail.com>
2023-03-17 19:58:56 +0530

    added instances salary and post

--- src/week4EmployeeManagementSystem/Employee.java
+++ src/week4EmployeeManagementSystem/Employee.java
@@ -6,17 +6,21 @@ public class Employee {
 	private String name;
 	private String address;
 	private String  phoneNo;
+	private String post;
+	private double salary;
 	
-	public Employee(int id ,String name , String address , String  phoneNo)
+	public Employee(int id ,String name , String address ,double salary, String post,String  phoneNo)
 	{
 		this.id = id;
 		this.name = name;
 		this.address = address;
 		this.phoneNo = phoneNo;
+		this.salary = salary;
+		this.post =post;
 	}
 	
 	public String toString()
 	{
-		return this.id+","+this.name+","+this.address+","+this.phoneNo;
+		return this.id+","+this.name+","+this.address+","+this.salary +","+this.post+","+this.phoneNo;
 	}
 }
