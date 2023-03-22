5c7136d3776173f8fb5c9883020a226e1748996d
abhaygupta3 <abhaygupta030902@gmail.com>
2023-03-22 21:01:01 +0530

    practicing file io

--- src/week4FileIO/Test.java
+++ src/week4FileIO/Test.java
@@ -1,22 +1,26 @@
 package week4FileIO;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 class Employee
 {
 	
-	int id;
+	String id;
 	String name;
 	String address;
-	long phoneNo;
+	String phoneNo;
 	
-	public Employee(int id ,String name , String address , long phoneNo)
+	public Employee(String id ,String name , String address , String phoneNo)
 	{
 		this.id = id;
 		this.name = name;
@@ -28,19 +32,89 @@ class Employee
 	{
 		return "["+this.id+","+this.name+","+this.address+","+this.phoneNo;
 	}
+	
+	public String getId() {
+		return this.id;
+	}
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public String getAddress()
+	{
+		return this.address;
+	}
+	
+	public String getPhone()
+	{
+		return this.phoneNo;
+	}
 }
 
 public class Test {
 
 	public static String filename = "D:\\\\softwares\\\\eclipse\\\\workspace\\\\NucleusTeqTraining\\\\src\\\\week4FileIO\\\\text.txt";
 
+	public Map<String, String> convertStringToHashMap(String line)
+	{
+		return Arrays
+				.stream(line.substring(1,line.length()-1).split(","))
+				.map(s -> s.split("=",2))
+				.collect(Collectors.toMap(s ->s[0].trim(), s->s[1].trim()));
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Employee e = new Employee(100 ,"Abhay","Basant Bihar",123456);
+		Employee e = new Employee("102" ,"Ashish sahu","Santoshi Nagar","123456");
+		
+		HashMap <String , String> map=new HashMap <String,String>();
+		
+		map.put("Job_id",e.getId());
+		map.put("Name", e.getName());
+		map.put("Address", e.getAddress());
+		map.put("PhoneNo", e.getPhone());
+		
+		//ArrayList<HashMap> arrayList = new ArrayList<HashMap>();
+		
+		//arrayList.add(map);
+		
+		System.out.println(map);
+		
+		try {
+			/*FileWriter fw = new FileWriter(filename,true);
+			fw.append(""+map);
+			fw.append("\n");
+			fw.close();*/
+			//fw.write((String)arrayList);
+			
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+			String line;
+			boolean check = true;
+			while((line=reader.readLine())!=null)
+			{
+				map = (HashMap<String, String>) new Test().convertStringToHashMap(line);
+				if(map.containsValue("1000"))
+				{
+					System.out.println(map);
+					check = false;
+				}
+			}
+			
+			if(check)
+			{
+				System.out.println("User not Found");
+			}
+			
+			reader.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 		//File f = new File(filename);
-		System.out.println(e);
+		/*System.out.println(e);
 		
 		try {
 			//FileWriter fw = new FileWriter(filename ,true);
@@ -53,7 +127,7 @@ public class Test {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+		*/
 	}
 
 }
